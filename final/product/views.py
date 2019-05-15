fac3741c7701ffb932f43442efdfc3e351ee2730
Farrah Lee <farrahyglee@yahoo.com>
2019-05-14 19:57:45 -0700

    added label

--- final/product/views.py
+++ final/product/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from . import models
 
-# Create your views here.
+#Import APIview for viewsets API interface
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class ProductView(APIView):
+    def get(self, request, format=None):
+        """ This creates a new channel """
+
+    def post(self, request, format=None):
+        """ This creates a new channel """
+
+class ProductTypeView(APIView):
+    def get(self, request, format=None, product_id=0):
+        """ This creates a new channel """
+
+    def post(self, request, format=None):
+        """ This creates a new channel """
+
+class ReviewView(APIView):
+    def get(self, request, format=None):
+        """ This creates a new channel """
+
+    def post(self, request, format=None):
+        """ This creates a new channel """
