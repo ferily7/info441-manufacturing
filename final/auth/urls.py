9960271bf4d4c88a9f3b5e753ff916a7b29e9609
Alex Huang <alexh5@uw.edu>
2019-06-06 16:25:14 -0700

    change purchase url

--- final/auth/urls.py
+++ final/auth/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
         path('signin', views.signin, name='signin'),
         path('signout', views.signout, name='signout'),
         path('register', views.register, name='register'),
-        path('purchases/', views.PurchaseView.as_view(), name='purchases'),
+        path('purchases', views.PurchaseView.as_view(), name='purchases'),
         path('profile', views.ProfileView.as_view(), name='profile')
 ]
