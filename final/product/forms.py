f9c430fe33623f3b4b190da627cde703c6be969b
Farrah Lee <farrahyglee@yahoo.com>
2019-06-07 22:28:05 -0700

    changed rating range

--- final/product/forms.py
+++ final/product/forms.py
@@ -1,7 +1,8 @@
 from django import forms
+from . import models
 
 class ReviewForm(forms.Form):
-    rating = forms.CharField(label='Rating', max_length=5)
+    rating = forms.IntegerField(min_value=1, max_value=5)
     description = forms.CharField(widget=forms.Textarea, max_length=250)
 
 class BrandForm(forms.Form):
@@ -13,3 +14,5 @@ class ProductForm(forms.Form):
     description = forms.CharField(widget=forms.Textarea, max_length=250)
     stock = forms.IntegerField()
     price = forms.DecimalField(max_digits=7, decimal_places=2)
+    # product_type = forms.ModelChoiceField(queryset= models.ProductType.objects.all(), to_field_name="name")
+    # brand = forms.ModelChoiceField(queryset= models.Brand.objects.all(), to_field_name="name")
